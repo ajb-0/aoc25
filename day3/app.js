b77d13ea6c6d80c03bf4b14fb90001fbd539355a
Adam Burgess <adam.burgess92@gmail.com>
2025-12-03 22:22:40 +1100

    Day 3 Solved

---
+++ day3/app.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+
+const file = "data.txt";
+const txt = fs.readFileSync(file, "utf8");
+const data = txt.split("\n")
+
+// Part 1
+function findIndexLargest(arr, start_idx, stop_idx) {
+    // Return index of largest number between start_idx and stop_idx.
+    let max = arr[start_idx];
+    let idx = start_idx;
+    for (let i=start_idx; i<=stop_idx; i++) {
+        // console.log(`idx: ${idx} | max: ${max} | arr[i]: ${arr[i]}`)
+        if (arr[i]>max) {
+            max = arr[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+
+function getMaxJoltage(s) {
+    let ns = s
+        .split("")
+        .map(x => Number(x))
+    a = findIndexLargest(ns, 0, ns.length-2)
+    b = findIndexLargest(ns, a+1, ns.length-1)
+    // console.log(`a: ${a} | b: ${b}`)
+    return Number(s[a])*10 + Number(s[b])
+}
+
+function solvePart1(data) {
+    let res = 0;
+    data.map(r => {res += getMaxJoltage(r)});
+    return res
+}
+console.log(solvePart1(data))
+
+// Part 2
+function calculateStopIndex(digit_idx, n_banks, n_required) {
+    // Given an array of length n_banks, find the index that 
+    // we must stop at when searching for the a'th digit in
+    // order to be able to form a number of length n_required.
+    return n_banks - n_required + digit_idx;
+}
+
+function getMaxJoltage12(s) {
+    let ns = s
+        .split("")
+        .map(x => Number(x))
+    let arr = [];
+    start_idx = 0;
+    for (let i=0; i<12; i++) {
+        stop_idx = calculateStopIndex(i, s.length, 12);
+        idx = findIndexLargest(ns, start_idx, stop_idx);
+        arr.push(s[idx]);
+        start_idx = idx+1;
+    }
+    return Number(arr.join(""));
+}
+
+function solvePart2(data) {
+    let res = 0;
+    data.map(r => {res += getMaxJoltage12(r)});
+    return res
+}
+console.log(solvePart2(data))
