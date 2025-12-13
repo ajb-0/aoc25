5cb3168cfe34c9349002eae2319604609292e988
Adam Burgess <adam.burgess92@gmail.com>
2025-12-13 21:15:47 +1100

    More day 8

--- day8/app.js
+++ day8/app.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
-const { cloneElement } = require("react");
+const { connect } = require("http2");
+const { register } = require("module");
+const { connected } = require("process");
 
 const file = "test_data.txt"
 const txt = fs.readFileSync(file, "utf8")
@@ -9,12 +11,8 @@ const data = txt
     .map(r => r.split(",").map(Number))
     
 function calcEuclidean(a, b) {
-    let x_a = a[0];
-    let y_a = a[1];
-    let z_a = a[2];
-    let x_b = b[0];
-    let y_b = b[1];
-    let z_b = b[2];
+    let x_a = a[0]; let y_a = a[1]; let z_a = a[2];
+    let x_b = b[0]; let y_b = b[1]; let z_b = b[2];
     return Math.sqrt(
         Math.pow((x_b - x_a), 2) +
         Math.pow((y_b - y_a), 2) +
@@ -22,72 +20,87 @@ function calcEuclidean(a, b) {
     )
 }
 
-// Make 10 shortest connections
-function shortestConnection(idx, data) {
-    let shortest = 0;
-    let retIdx = 0;
+function calcAllDistances(data) {
+    out = []
+    console.log(data)
     for (let i=0; i<data.length; i++) {
-        if (i===idx) {
-            continue
+        let a = data[i]
+        for (let j=0; j<data.length; j++) {
+            if (i === j) {
+                continue
+            }
+            let b = data[j];
+            out.push([a, b, calcEuclidean(a, b)])
         }
-        let dist = calcEuclidean(data[idx], data[i]);
-        if (shortest === 0) {
-            shortest = dist;
-        } else if (dist < shortest) {
-            shortest = dist;
-            retIdx = i;
-        }
-        // console.log(`${data[idx]} | ${i} | ${data[i]} | dist: ${dist} | shortest: ${shortest} | retIdx: ${retIdx}`)
-    }
-    return [shortest, idx, retIdx];
-}
-
-// console.log(shortestConnection(0, data))
-
-function getAllDistances(data) {
-    let out = []
-    for (let i=0; i<data.length; i++) {
-        out.push(shortestConnection(i, data))
     }
     return out;
 }
-let distanceData = getAllDistances(data);
 
-function sortByFirstCol(a, b) {
-    if (a[0] === b[0]) {
-        return 0;
-    } else {
-        return (a[0] < b[0]) ? -1 : 1;
+function sortThirdCol(a, b) {
+    if (a[2] === b[2]) {
+        return 0
+    }
+    if (a[2] < b[2]) {
+        return -1
+    }
+    if (a[2] > b[2]) {
+        return 1
     }
 }
 
-function sortDistances(data) {
-    data.sort(sortByFirstCol)
-}
-sortDistances(distanceData)
-console.log(distanceData)
+// function makeConnections(distances, n) {
+//     let n_connections = 0;
+//     let connections = []
+//     for (let i=0; i<distances.length; i++) {
+//         let a = distances[i][0];
+//         let b = distances[i][1];
+//         if (i===0) {
+//             connections.push([distances[i][0], distances[i][1]]);
+//             continue
+//         }
+//         for (let j=0; j<connections.length; j++) {
+//             if (connections[j].some())
 
-function removeDupes(data) {
-  const seen = new Set();
-  const out = [];
+//         }
+//         n_connections++;
+//         console.log(n_connections)
+//         if (n_connections === n) {
+//             return connections;
+//         }
+//     }
+// }
 
-  for (const [dist, a, b] of data) {
-    const key1 = `${a}-${b}`;
-    const key2 = `${b}-${a}`;
 
-    if (!seen.has(key1) && !seen.has(key2)) {
-      seen.add(key1);
-      out.push([dist, a, b]);
-    }
-  }
+// function solvePart1(data) {
+//     let distances = calcAllDistances(data);
+//     distances.sort(sortThirdCol);
+//     console.log(distances)
+// }
 
-  return out;
-}
-let dupesRemoved = removeDupes(distanceData);
 
-function getTen(dataDupesRemoved) {
-    return dataDupesRemoved.slice(0, 10);
-}
 
-let firstTen = getTen(dupesRemoved);
-console.log(firstTen);
+
+
+
+
+console.log(solvePart1(data))
+
+
+let d = [
+  [ [ 162, 817, 812 ], [ 425, 690, 689 ], 316.90219311326956 ],
+  [ [ 425, 690, 689 ], [ 162, 817, 812 ], 316.90219311326956 ],
+  [ [ 162, 817, 812 ], [ 431, 825, 988 ], 321.560258738545 ],
+  [ [ 431, 825, 988 ], [ 162, 817, 812 ], 321.560258738545 ],
+  [ [ 906, 360, 560 ], [ 805, 96, 715 ], 322.36935338211043 ],
+  [ [ 805, 96, 715 ], [ 906, 360, 560 ], 322.36935338211043 ],
+  [ [ 431, 825, 988 ], [ 425, 690, 689 ], 328.11888089532425 ],
+  [ [ 425, 690, 689 ], [ 431, 825, 988 ], 328.11888089532425 ],
+  [ [ 862, 61, 35 ], [ 984, 92, 344 ], 333.6555109690233 ],
+  [ [ 984, 92, 344 ], [ 862, 61, 35 ], 333.6555109690233 ],
+  [ [ 52, 470, 668 ], [ 117, 168, 530 ], 338.33858780813046 ],
+  [ [ 117, 168, 530 ], [ 52, 470, 668 ], 338.33858780813046 ],
+  [ [ 819, 987, 18 ], [ 941, 993, 340 ], 344.3893145845266 ]
+]
+
+let c = makeConnections(d, 3)
+console.log(c)
